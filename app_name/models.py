247a403258dd97d2d09af6331be11e1c80367ac9
Jared <jarcoal@gmail.com>
2012-07-24 17:28:12 -0700

    add randkey

--- app_name/models.py
+++ app_name/models.py
@@ -8,3 +8,7 @@ from imagekit.imagecache import NonValidatingImageCacheBackend
 
 #utils
 from django.template.defaultfilters import slugify
+import time
+
+#useful for creating unique, hash-friendly IDs for image uploads
+randkey = lambda: str(int(time.time() * 1000000))[::-1]
