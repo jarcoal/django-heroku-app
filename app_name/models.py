f47fd1360dd00c76c07f18113a4a30190a2d3c71
Jared <jarcoal@gmail.com>
2012-07-24 17:12:31 -0700

    add imagekit imports to models file

--- app_name/models.py
+++ app_name/models.py
@@ -1,3 +1,10 @@
+#models
 from django.db import models
 
-# Create your models here.
+#imagekit
+from imagekit.models import ProcessedImageField, ImageSpecField
+from imagekit.processors import resize, crop
+from imagekit.imagecache import NonValidatingImageCacheBackend
+
+#utils
+from django.template.defaultfilters import slugify
