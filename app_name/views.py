9244bf9d9fda1d78572abcd243d240a358844ba1
Jared <jarcoal@gmail.com>
2012-07-24 17:14:03 -0700

    add some imports to views

--- app_name/views.py
+++ app_name/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+#base views
+import django.views.generic as generic
+
+#http
+from django.http import HttpResponse, HttpResponseRedirect
+
+#utils
+from django.core.urlresolvers import reverse
